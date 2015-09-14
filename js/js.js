c7b2735133cfb5fd09f979655195f7f0c4dd64c1
JoeyQiang <joeyqiang@gmail.com>
2015-09-14 08:19:14 -0500

    Change from PC
    Fix the conflicts in the office PC

--- js/js.js
+++ js/js.js
@@ -1,29 +1,35 @@
-$(function() {
-	var slideToggle = {
-		$target: $(".date"),
-		$slide_content: $(".detail"),
-		$cur_width: $(window).width(),
-		initial: function() {
-			if (this.$cur_width < 1024) {
-				this.$slide_content.eq(0).css({
-					display: 'block'
-				});
-				this.bind(this.$target);
-			}
-		},
-		bind: function($o) {
-			$o.bind("click", function() {
-				var $detail = $(this).next('.detail');
-				slideToggle.animate($detail);
-			});
-		},
-		animate: function($o) {
-			if ($o.is(":hidden")) {
-				$o.slideDown('slow');
-			} else {
-				$o.slideUp('slow');
-			}
+(function($) {
+	$(function() {
+		function SlideToggle(date, detail) {
+			this.target = $("." + date);
+			this.slide_content = $("." + detail);
 		}
-	};
-	slideToggle.initial();
-});
+		SlideToggle.prototype = {
+			cur_width: $(window).width(),
+			initial: function() {
+				if (this.cur_width < 1024) {
+					this.slide_content.eq(0).css({
+						display: 'block'
+					});
+					this.bind(this.target);
+				}
+			},
+			bind: function($o) {
+				var _this = this;
+				$o.bind("click", function() {
+					var $detail = $(this).next('.detail');
+					_this.animate($detail);
+				});
+			},
+			animate: function($o) {
+				if ($o.is(":hidden")) {
+					$o.slideDown('slow');
+				} else {
+					$o.slideUp('slow');
+				}
+			}
+		};
+		var slideToggle = new SlideToggle("date", "detail");
+		slideToggle.initial();
+	});
+})(jQuery);
